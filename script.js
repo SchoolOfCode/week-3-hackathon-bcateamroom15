973ce053b5e1a85c9cf09303bac32ad781f9ba2f
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2024-12-12 12:29:04 +0000

    created some of the pseudocode for the game page

---
+++ script.js
@@ -0,0 +1,20 @@
+//Instructions Page
+
+//create a welcome/instruction page which will explain the trivia challenge which is on films, multiple choice, and that we have a points system. - html file using p
+//create a play button (hyperlink that will take you to the trivia challenge game). So we need two html page.
+
+//Pseudocode for game page
+
+//html object which will be our trivia questions - this will show one quetsion at a time
+//5 buttons including the skip one - 4 for multiple choice
+//fetch API on js file
+//create async function which fetches the API
+//access each specific question at a time,
+//access the correct answers and the incorrect answers,
+//log the correct and incorrect answers in random order for the user to see,
+//present buttons for the user to choose 'try again'
+
+
+
+//we need to display the correct answer once the has picked a response
+
